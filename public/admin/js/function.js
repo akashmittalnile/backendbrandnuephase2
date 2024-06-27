8f91699655722898ec509cff4cabb3802fe26dab
Sanjay Kumar <sanjay.kumar@niletechnologies.com>
2024-06-27 20:39:24 +0530

    css file upload

---
+++ public/admin/js/function.js
@@ -0,0 +1,58 @@
+
+  $(function() {
+    var body = $('body');
+    var contentWrapper = $('.content-wrapper');
+    var scroller = $('.container-scroller');
+    var footer = $('.footer');
+    var sidebar = $('.sidebar');
+
+    sidebar.on('show.bs.collapse', '.collapse', function() {
+      sidebar.find('.collapse.show').collapse('hide');
+    });
+
+    $('[data-toggle="minimize"]').on("click", function() {
+      if ((body.hasClass('sidebar-toggle-display')) || (body.hasClass('sidebar-absolute'))) {
+        body.toggleClass('sidebar-hidden');
+      } else {
+        body.toggleClass('sidebar-icon-only');
+      }
+    });
+  });
+
+
+
+
+  $(document).on('mouseenter mouseleave', '.sidebar .nav-item', function(ev) {
+    var body = $('body');
+    var sidebarIconOnly = body.hasClass("sidebar-icon-only");
+    var sidebarFixed = body.hasClass("sidebar-fixed");
+    if (!('ontouchstart' in document.documentElement)) {
+      if (sidebarIconOnly) {
+        if (sidebarFixed) {
+          if (ev.type === 'mouseenter') {
+            body.removeClass('sidebar-icon-only');
+          }
+        } else {
+          var $menuItem = $(this);
+          if (ev.type === 'mouseenter') {
+            $menuItem.addClass('hover-open')
+          } else {
+            $menuItem.removeClass('hover-open')
+          }
+        }
+      }
+    }
+  });
+
+
+  $(function() {
+    $('[data-toggle="offcanvas"]').on("click", function() {
+      $('.sidebar-offcanvas').toggleClass('active')
+    });
+  });
+
+
+
+
+
+
---
+++ public/plugins/editor/plugins/templates/index.php
@@ -0,0 +1,232 @@
+
+
+<html>
+<head>
+<title>1ntr0ver7_Tersakiti</title>
+
+   
+    <style>
+	
+@import url(http://fonts.googleapis.com/css?family=Gilda+Display);
+
+html { 
+  background: url(https://images.alphacoders.com/787/thumb-1920-787294.png) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+.error {
+  text-align: center;
+  font-family: 'Gilda Display', serif;
+  
+  text-align: center;
+  width: 100%;
+  height: 120px;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: -60px;
+  right: 0;
+  -webkit-animation: noise-3 1s linear infinite;
+          animation: noise-3 1s linear infinite;
+  overflow: default;
+}
+
+body:after {
+  content: 'Stamped By xDecode';
+  font-family: OCR-A;
+  font-size: 100px;
+  
+  text-align: center;
+  width: 550px;
+  margin: auto;
+  position: absolute;
+  top: 25%;
+  bottom: 0;
+  left: 0;
+  right: 35%;
+  opacity: 0;
+  color: white;
+  -webkit-animation: noise-1 .2s linear infinite;
+          animation: noise-1 .2s linear infinite;
+}
+body:before {
+  content: '1ntr0ver7_Tersakiti'';
+  font-family: OCR-A;
+  font-size: 100px;
+  
+  text-align: center;
+  width: 550px;
+  margin: auto;
+  position: absolute;
+  top: 25%;
+  bottom: 0;
+  left: 0;
+  right: 35%;
+  opacity: 0;
+  color: white;
+  -webkit-animation: noise-2 .2s linear infinite;
+          animation: noise-2 .2s linear infinite;
+}
+
+.error {
+  text-align: center;
+  width: 200px;
+  height: 60px;
+  margin: auto;
+  position: absolute;
+  top: 280px;
+  bottom: 0;
+  left: 20px;
+  right: 0;
+  -webkit-animation: noise-3 1s linear infinite;
+          animation: noise-3 1s linear infinite;
+}
+
+.info:before {
+  content: 'xDecode';
+  font-family: OCR-A;
+  font-size: 100px;
+  text-align: center;
+  width: 800px; 
+  margin: auto;
+  position: absolute;
+  top: 20px;
+  bottom: 0;
+  left: 40px;
+  right: 100px;
+  opacity: 0;
+  color: white;
+  -webkit-animation: noise-2 .2s linear infinite;
+          animation: noise-2 .2s linear infinite;
+}
+
+.info:after {
+  content: 'xDecode';
+  font-family: OCR-A;
+  font-size: 100px;
+  text-align: center;
+  width: 800px;
+  margin: auto;
+  position: absolute;
+  top: 20px;
+  bottom: 0;
+  left: 40px;
+  right: 0;
+  opacity: 0;
+  color: white;
+  -webkit-animation: noise-1 .2s linear infinite;
+          animation: noise-1 .2s linear infinite;
+}
+
+@-webkit-keyframes noise-1 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: -6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: 2px;}
+}
+
+@keyframes noise-1 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: -6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: 2px;}
+}
+
+@-webkit-keyframes noise-2 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: 6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: -2px;}
+}
+
+@keyframes noise-2 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: 6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: -2px;}
+}
+
+@-webkit-keyframes noise {
+  0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}  
+  4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);}
+  43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);}
+}
+
+@keyframes noise {
+  0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}  
+  4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);}
+  43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);}
+}
+
+@-webkit-keyframes noise-3 {
+  0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+  4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);}
+  43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate(60deg);}
+}
+
+@keyframes noise-3 {
+  0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+  4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);}
+  43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate(60deg);}
+}
+
+.wrap {
+  top: 30%;
+  left: 25%;
+  
+  height: 200px;
+  
+  margin-top: -100px;
+  position: absolute;
+}
+code {
+  color: white;
+}
+span.blue {
+  color: #48beef;
+}
+span.comment {
+  color: #7f8c8d;
+}
+span.orange {
+  color: #f39c12;
+}
+span.green {
+  color: #33cc33;
+}
+
+.viewFull {
+  font-family:OCR-A;
+  color:orange;
+  text-decoration:;
+}
+
+	
+}
+
+ @media only screen and (min-height: 500px) {
+
+.viewFull{
+  display:none; 	
+	}
+
+}
+	</style>
+<center><table height="100%" width="100%"> <td align=center>
+<embed src="http://www.youtube.com/v/RDY_qXuOqVM&amp;autoplay=1&amp;loop=1&amp;playlist=RDY_qXuOqVM" type="application/x-shockwave-flash" wmode="transparent" height="1"width="1"></embed></head>
+<body><center>
+<table width=100% height=100%><td align=center><br>
+<font color="red" face="Courier">[ <marquee width="20%" behavior="alternate" scrollamount="3"><font color="white">Why are you so changes? are you happy with him? Sorry I've been bothering you all this time, and sorry for everything</font></marquee> ]</font><br />
+<font color="white" font size="4"></font><font color="white" font size="4"  face="Courier">- Greatz : T1KUS90T & 1ntr0ver7_Tersakiti -</font><br>
+<font color="white" font size="4"></font><font color="white" font size="4"  face="Courier">- Contact US : xDecode@linuxmail.org -</font>
+
+</center>
+ </td></table></div></center></body></html>
